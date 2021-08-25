a1af0a35fd2371b1d6aef407b42f8f4965061f4d
Matt Pickles <mattwpickles@gmail.com>
2021-08-25 11:42:14 +0100

    Push public files

---
+++ web/app/themes/wraith/public/scripts/customizer.js
@@ -0,0 +1,2 @@
+(self.webpackChunk=self.webpackChunk||[]).push([[438],{184:function(n,t,u){var e=u(609);wp.customize("blogname",(function(n){n.bind((function(n){return e(".brand").text(n)}))}))},609:function(n){"use strict";n.exports=window.jQuery}},function(n){"use strict";var t;t=184,n(n.s=t)}]);
+//# sourceMappingURL=customizer.js.map
---
+++ web/app/themes/wraith/public/scripts/editor.js
@@ -0,0 +1,2 @@
+(self.webpackChunk=self.webpackChunk||[]).push([[804],{448:function(e,t,n){"use strict";window.wp.editPost;var o=window.wp.domReady,i=n.n(o),u=window.wp.blocks;i()((function(){(0,u.unregisterBlockStyle)("core/button","outline"),(0,u.registerBlockStyle)("core/button",{name:"outline",label:"Outline"})}))}},function(e){"use strict";var t;t=448,e(e.s=t)}]);
+//# sourceMappingURL=editor.js.map
