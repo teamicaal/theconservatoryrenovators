747428327502ca79c5d074f222c76465db05cbed
Matt Pickles <mattwpickles@gmail.com>
2021-08-26 18:33:51 +0100

    Templates

--- web/app/themes/wraith/resources/views/template-text.blade.php
+++ web/app/themes/wraith/resources/views/template-text.blade.php
@@ -1,11 +1,11 @@
 {{--
-  Template Name: Custom Template
+  Template Name: Simple Text
 --}}
 
 @extends('layouts.app')
 
 @section('content')
-  @while(have_posts()) @php(the_post())
+  @while(have_posts()) @php the_post() @endphp
     @include('partials.page-header')
     @include('partials.content-page')
   @endwhile
