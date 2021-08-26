f7f01070701cfb8bef6eddc547a92142440a4c2e
Matt Pickles <mattwpickles@gmail.com>
2021-08-26 14:52:45 +0100

    Image sizes & lozad blur

--- web/app/themes/wraith/app/image-sizes.php
+++ web/app/themes/wraith/app/image-sizes.php
@@ -1,4 +1,5 @@
 <?php
+add_image_size( 'lozad', 150, 0, true );
 add_image_size( '4by3-sm', 600, 450, true );
 add_image_size( '4by3-md', 800, 600, true );
 add_image_size( '4by3-lg', 1200, 900, true );
--- web/app/themes/wraith/resources/views/partials/flexible-content/banner.blade.php
+++ web/app/themes/wraith/resources/views/partials/flexible-content/banner.blade.php
@@ -15,9 +15,20 @@ $paragraph = get_sub_field('paragraph');
   <div class="lg:w-3/5 order-1 md:order-2 relative z-0">
     {!! count($images) > 1 ? '<div class="slick-banner relative z-10" style="top: 7px;">' : null; !!}
     @foreach($images as $key=>$image )
+    @php
+      $img_webp = str_replace('jpg', 'webp', $image['url']);
+      $img_webp_sm = str_replace('.jpg', '-sm.jpg', $img_webp);
+    @endphp
       <div>
         <div class="relative banner-height {!! $key != 0 ? 'hidden' : null !!}" style="min-height: 75vh;">
-            <img src="{{ $image['url'] }}" srcset="{!! wp_get_attachment_image_srcset($image['id']) !!}" class="absolute object-cover-absolute w-full h-full inset-0" width="100%" height="100%">
+          <picture>
+            @if( filter_var($img_webp_sm, FILTER_VALIDATE_URL) == TRUE )
+              <source media="(max-width: 767px)" data-srcset="{!! $img_webp_sm !!}" />
+            @elseif( filter_var($img_webp, FILTER_VALIDATE_URL) == TRUE )
+              <source media="(min-width: 768px)" data-srcset="{!! $img_webp !!}" />
+            @endif
+            <img src="{{ $image['sizes']['lozad'] }}" data-srcset="{!! wp_get_attachment_image_srcset($image['id']) !!}" class="lozad absolute object-cover-absolute w-full h-full inset-0" width="100%" height="100%">
+          </picture>
         </div>
       </div>
     @endforeach
