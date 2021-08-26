e32cd93b74da4fee06d56b260c40e1c9473cbed5
Matt Pickles <mattwpickles@gmail.com>
2021-08-26 17:46:00 +0100

    Passive scroll

--- web/app/themes/wraith/resources/scripts/app.js
+++ web/app/themes/wraith/resources/scripts/app.js
@@ -2,6 +2,26 @@
  * External Dependencies
  */
 import 'jquery';
+jQuery.event.special.touchstart = {
+  setup: function( _, ns, handle ) {
+    this.addEventListener("touchstart", handle, { passive: !ns.includes("noPreventDefault") });
+  }
+};
+jQuery.event.special.touchmove = {
+  setup: function( _, ns, handle ) {
+    this.addEventListener("touchmove", handle, { passive: !ns.includes("noPreventDefault") });
+  }
+};
+jQuery.event.special.wheel = {
+  setup: function( _, ns, handle ){
+    this.addEventListener("wheel", handle, { passive: true });
+  }
+};
+jQuery.event.special.mousewheel = {
+  setup: function( _, ns, handle ){
+    this.addEventListener("mousewheel", handle, { passive: true });
+  }
+};
 
 import lozad from 'lozad';
 const observer = lozad();
