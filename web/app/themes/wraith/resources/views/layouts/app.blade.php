17cf0e32fe56c50e3c2f86f853c76a238be005d6
Matt Pickles <mattwpickles@gmail.com>
2021-09-22 15:15:12 +0100

    Fix duplicate js actions

--- web/app/themes/wraith/resources/views/layouts/app.blade.php
+++ web/app/themes/wraith/resources/views/layouts/app.blade.php
@@ -1,8 +1,6 @@
 <!doctype html>
 <html {!! get_language_attributes() !!}>
-  @include('partials.head')
   <body @php body_class() @endphp>
-    @php do_action('get_header') @endphp
     @include('partials.header')
     <div class="wrap" role="document">
       <div class="content">
@@ -16,9 +14,7 @@
         @endif
       </div>
     </div>
-    @php do_action('get_footer') @endphp
     @include('partials.footer')
-    @php wp_footer() @endphp
     @php
     $script_footer = get_field('script_footer', 'option');
     @endphp
