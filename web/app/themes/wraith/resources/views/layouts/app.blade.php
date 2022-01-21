025f79165a0a98c4c093d0886fb2010bc3af53c9
aidenicaal <53270393+aidenicaal@users.noreply.github.com>
2022-01-21 13:50:32 +0000

    fix duplicate html

--- web/app/themes/wraith/resources/views/layouts/app.blade.php
+++ web/app/themes/wraith/resources/views/layouts/app.blade.php
@@ -1,23 +1,18 @@
-<!doctype html>
-<html {!! get_language_attributes() !!}>
-  <body @php body_class() @endphp>
-    @include('partials.header')
-    <div class="wrap" role="document">
-      <div class="content">
-        <main class="main">
-          @yield('content')
-        </main>
-        @hasSection('sidebar')
-          <aside class="sidebar">
-            @yield('sidebar')
-          </aside>
-        @endif
-      </div>
-    </div>
-    @include('partials.footer')
-    @php
-    $script_footer = get_field('script_footer', 'option');
-    @endphp
-    {!! $script_footer ? $script_footer : null !!}
-  </body>
-</html>
+@include('partials.header')
+<div class="wrap" role="document">
+  <div class="content">
+    <main class="main">
+      @yield('content')
+    </main>
+    @hasSection('sidebar')
+      <aside class="sidebar">
+        @yield('sidebar')
+      </aside>
+    @endif
+  </div>
+</div>
+@include('partials.footer')
+@php
+$script_footer = get_field('script_footer', 'option');
+@endphp
+{!! $script_footer ? $script_footer : null !!}
