68958fccb1cfa16a8b3f18da1c3b5174f45198e7
Matt Pickles <mattwpickles@gmail.com>
2022-02-21 13:14:09 +0000

    Update accreditations-row.blade.php

--- web/app/themes/wraith/resources/views/partials/flexible-content/accreditations-row.blade.php
+++ web/app/themes/wraith/resources/views/partials/flexible-content/accreditations-row.blade.php
@@ -9,7 +9,19 @@ $custom_class = get_sub_field('custom_class');
   </div>
   <div class="container mx-auto relative px-0 lg:px-1">
     @if( have_rows('accreditations') )
-
+      @while( have_rows('accreditations') )
+        @php
+        the_row();
+        $logo = get_sub_field('logo');
+        $link = get_sub_field('link');
+        $alt = get_sub_field('image_alt');
+        @endphp
+        <div class="w-10 lg:w-1/6 mb-1 px-1 snap-center" style="flex: 0 0 auto;">
+          <{!! $link ? 'a href="' . $link . '" target="_blank"' : 'div'; !!} class="block bg-white rounded-sm border border-grey-light p-1 lg:p-1-5 text-center">
+            <img data-src="{{ $logo['url'] }}" src="{{ $logo['sizes']['thumbnail'] }}" width="96" height="96" alt="{{ $alt ? $alt : $logo['alt'] }}" class="lozad noblur w-full h-auto mx-auto" style="max-width: 6rem;" />
+          </{!! $link ? 'a' : 'div'; !!}>
+        </div>
+      @endwhile
     @elseif( have_rows('accreditations', 'option') )
       <div class="flex lg:flex-wrap lg:justify-center snap-x overflow-x-scroll lg:overflow-hidden lg:-mx-1">
         @while( have_rows('accreditations', 'option') )
@@ -17,11 +29,10 @@ $custom_class = get_sub_field('custom_class');
           the_row();
           $logo = get_sub_field('logo');
           $link = get_sub_field('link');
-          $alt = get_sub_field('image_alt');
           @endphp
           <div class="w-10 lg:w-1/6 mb-1 px-1 snap-center" style="flex: 0 0 auto;">
             <{!! $link ? 'a href="' . $link . '" target="_blank"' : 'div'; !!} class="block bg-white rounded-sm border border-grey-light p-1 lg:p-1-5 text-center">
-              <img data-src="{{ $logo['url'] }}" src="{{ $logo['sizes']['thumbnail'] }}" width="96" height="96" alt="{{ $alt ? $alt : $logo['alt'] }}" class="lozad noblur w-full h-auto mx-auto" style="max-width: 6rem;" />
+              <img data-src="{{ $logo['url'] }}" src="{{ $logo['sizes']['thumbnail'] }}" width="96" height="96" alt="{{ $logo['alt'] ? $logo['alt'] : null }}" class="lozad noblur w-full h-auto mx-auto" style="max-width: 6rem;" />
             </{!! $link ? 'a' : 'div'; !!}>
           </div>
         @endwhile
