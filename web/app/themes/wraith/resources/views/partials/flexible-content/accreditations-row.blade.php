1d9a7e2317c3c566b59f4e7ddd1c22eb44940e96
Matt Pickles <mattwpickles@gmail.com>
2022-02-21 13:02:22 +0000

    Update Tailwind > 3

--- web/app/themes/wraith/resources/views/partials/flexible-content/accreditations-row.blade.php
+++ web/app/themes/wraith/resources/views/partials/flexible-content/accreditations-row.blade.php
@@ -11,14 +11,14 @@ $custom_class = get_sub_field('custom_class');
     @if( have_rows('accreditations') )
 
     @elseif( have_rows('accreditations', 'option') )
-      <div class="flex lg:flex-wrap lg:justify-center scroll-x-proximity overflow-x-scroll lg:overflow-hidden lg:-mx-1">
+      <div class="flex lg:flex-wrap lg:justify-center snap-x overflow-x-scroll lg:overflow-hidden lg:-mx-1">
         @while( have_rows('accreditations', 'option') )
           @php
           the_row();
           $logo = get_sub_field('logo');
           $link = get_sub_field('link');
           @endphp
-          <div class="w-10 lg:w-1/6 mb-1 px-1 scroll-align-start" style="flex: 0 0 auto;">
+          <div class="w-10 lg:w-1/6 mb-1 px-1 snap-center" style="flex: 0 0 auto;">
             <{!! $link ? 'a href="' . $link . '" target="_blank"' : 'div'; !!} class="block bg-white rounded-sm border border-grey-light p-1 lg:p-1-5 text-center">
               <img data-src="{{ $logo['url'] }}" src="{{ $logo['sizes']['thumbnail'] }}" width="96" height="96" alt="{{ $logo['alt'] }}" class="lozad noblur w-full h-auto mx-auto" style="max-width: 6rem;" />
             </{!! $link ? 'a' : 'div'; !!}>
--- web/app/themes/wraith/resources/views/partials/flexible-content/page-links-simple.blade.php
+++ web/app/themes/wraith/resources/views/partials/flexible-content/page-links-simple.blade.php
@@ -9,7 +9,7 @@ $subtitle = get_sub_field('subtitle');
       {!! $subtitle ? '<h4 class="title-display title-display-center text-primary mb-1">' . $subtitle . '</h4>' : null; !!}
       {!! $title ? '<h2 class="text-4xl mb-2">' . $title . '</h2>' : null; !!}
       @if( have_rows('pages') )
-      <div class="flex lg:flex-wrap lg:justify-center scroll-x-proximity overflow-x-scroll lg:overflow-hidden lg:-mx-1">
+      <div class="flex lg:flex-wrap lg:justify-center snap-x overflow-x-scroll lg:overflow-hidden lg:-mx-1">
         @while( have_rows('pages') )
           @php
           the_row();
@@ -17,7 +17,7 @@ $subtitle = get_sub_field('subtitle');
           $custom_title = get_sub_field('custom_title');
           @endphp
           @if( $page )
-            <div class="w-16 lg:w-1/3 mb-1 px-1 scroll-align-start mb-2" style="flex: 0 0 auto;">
+            <div class="w-16 lg:w-1/3 mb-1 px-1 snap-center mb-2" style="flex: 0 0 auto;">
               <a href="{{ get_the_permalink($page->ID) }}" class="embed embed-4by3 rounded-sm mb-1">
                 <img data-src="{{ get_the_post_thumbnail_url($page->ID) }}" src="{{ get_the_post_thumbnail_url($page->ID, 'lozad') }}" alt="{!! $custom_title ? $custom_title : $page->title; !!}" class="lozad object-cover-absolute" />
               </a>
