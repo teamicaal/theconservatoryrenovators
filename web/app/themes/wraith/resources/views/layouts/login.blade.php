b9e52dca58110f0ec4227f62b70562fca45689ee
Matt Pickles <mattwpickles@gmail.com>
2022-03-04 18:59:09 +0000

    Mobile login body fix

--- web/app/themes/wraith/resources/views/layouts/login.blade.php
+++ web/app/themes/wraith/resources/views/layouts/login.blade.php
@@ -3,7 +3,7 @@
   @include('partials.head')
   @php $background = get_field('background_login', 'option') @endphp
   {!! $background ? '<link rel="preload" src="' . $background['sizes']['lozad'] . '" type="image/jpg" />' : null !!}
-  <body @php body_class('bg-grey-lightest') @endphp>
+  <body @php body_class('bg-grey-lightest relative') @endphp>
     @php do_action('get_header') @endphp
     @if( $background )
       <img src="{{ $background['sizes']['lozad'] }}" data-src="{{ $background['url'] }}" alt="Login" class="lozad absolute w-full h-full inset-0 object-cover duration-1000" />
