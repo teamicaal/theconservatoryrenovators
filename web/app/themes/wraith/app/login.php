4d9fe994c3ad1d0905e2a15292043bd55bfe0cc3
Matt Pickles <mattwpickles@gmail.com>
2022-03-04 18:38:28 +0000

    Custom login page

---
+++ web/app/themes/wraith/app/login.php
@@ -0,0 +1,65 @@
+<?php
+  // Setup login page
+  function icaal_login_page( $login_url, $redirect ) {
+    return home_url( '/wr-admin/' );
+  }
+  add_filter( 'login_url', 'icaal_login_page', 10, 2 );
+
+  // Redirect wp-login.php to new login page
+  function icaal_redirect_login_page() {
+    $login_page  = home_url( '/wr-admin/' );
+    $page_viewed = basename($_SERVER['REQUEST_URI']);
+
+    if( $page_viewed == "wp-login.php" && $_SERVER['REQUEST_METHOD'] == 'GET') {
+      wp_safe_redirect($login_page);
+      exit;
+    }
+  }
+  add_action('init', 'icaal_redirect_login_page');
+
+  // Add failed notice
+  function icaal_login_failed() {
+    $login_page  = home_url( '/wr-admin/' );
+    wp_redirect( $login_page . '?login=failed' );
+    exit;
+  }
+  add_action( 'wp_login_failed', 'icaal_login_failed' );
+
+  // Add empty user/pass notice
+  function icaal_verify_username_password( $user, $username, $password ) {
+    $login_page  = home_url( '/wr-admin/' );
+    if( $username == "" || $password == "" ) {
+      wp_redirect( $login_page . "?login=empty" );
+      exit;
+    }
+  }
+  add_filter( 'authenticate', 'icaal_verify_username_password', 1, 3);
+
+  // Add logout notice
+  function icaal_logout_page() {
+    $login_page  = home_url( '/wr-admin/' );
+    wp_redirect( $login_page . "?login=false" );
+    exit;
+  }
+  add_action('wp_logout', 'icaal_logout_page');
+
+  // Redirect on successful login
+  function icaal_login_redirect( $redirect_to, $request, $user ) {
+    // is there a user to check?
+    if ( isset( $user->roles ) && is_array( $user->roles ) ) {
+      //check for admins
+      if ( in_array( 'subscriber', $user->roles ) ) {
+        // Redirect to the media centre page
+        return get_post_type_archive_link('media');
+      } elseif( $redirect_to ) {
+        return $redirect_to;
+      } else {
+        return admin_url();
+      }
+    } else {
+      return home_url();
+    }
+  }
+  add_filter( 'login_redirect', 'icaal_login_redirect', 10, 3 );
+
+?>
---
+++ web/app/themes/wraith/resources/views/partials/form-login.blade.php
@@ -0,0 +1,30 @@
+<?php
+
+	$redirect_to  = (isset($_GET['redirect_to']) ) ? $_GET['redirect_to'] : 0;
+
+?>
+<form name="loginform" id="loginform" action="<?php echo home_url(); ?>/wp/wp-login.php" method="post">
+	<fieldset class="login-username mb-2">
+		<label for="user_login" class="block text-sm mb-0-25 cursor-pointer">Username</label>
+		<input class="w-full border border-grey-lighter rounded p-1" type="text" name="log" id="user_login" class="form-control" value="" size="20" placeholder="Enter your username">
+	</fieldset>
+	<p class="login-password mb-2">
+		<label for="user_pass" class="block text-sm mb-0-25 cursor-pointer">Password</label>
+		<input class="w-full border border-grey-lighter rounded p-1" type="password" name="pwd" id="user_pass" class="form-control" value="" size="20" placeholder="Enter your password">
+	</fieldset>
+	<div class="lg:flex lg:justify-between items-center">
+    <div class="login-remember switch pull-xs-left mb-1 lg:mb-0">
+  		<label class="form-control-label cursor-pointer">
+  			<input name="rememberme" type="checkbox" id="rememberme" value="forever">
+  			<span class="indicator"></span>
+  			<span class="label">Remember Me</span>
+  		</label>
+  	</div>
+  	<div class="login-submit text-xs-right">
+  		<input type="submit" name="wp-submit" id="wp-submit" class="btn bg-primary text-white hover:bg-primary-dark block lg:inline-block w-full lg:w-auto cursor-pointer" value="Log In">
+  		<?php if( $redirect_to ): ?>
+  		<input type="hidden" name="redirect_to" value="<?php echo $redirect_to ?>">
+  		<?php endif; ?>
+  	</div>
+  </div>
+</form>
