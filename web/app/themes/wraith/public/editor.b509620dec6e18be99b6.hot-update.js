9ea3924a95cb7126409bc90effe9b88749638cd0
rgvaghani <33823707+rgvaghani@users.noreply.github.com>
2022-09-23 10:04:58 +0100

    build

---
+++ web/app/themes/wraith/public/editor.b509620dec6e18be99b6.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdatesage"]("editor",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "5971cb506e6b8b2622ee"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=editor.b509620dec6e18be99b6.hot-update.js.map
--- web/app/themes/wraith/public/js/editor.js
+++ web/app/themes/wraith/public/js/editor.js
@@ -4657,7 +4657,7 @@ function ansiRegex({onlyFirst = false} = {}) {
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b509620dec6e18be99b6"; }
+/******/ 		__webpack_require__.h = function() { return "5971cb506e6b8b2622ee"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
