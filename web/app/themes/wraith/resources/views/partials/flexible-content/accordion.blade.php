d72d44d13fbb66488fa76e3125d362fc997c0dbe
rgvaghani <33823707+rgvaghani@users.noreply.github.com>
2022-09-23 11:26:57 +0100

    Update accordion.blade.php

--- web/app/themes/wraith/resources/views/partials/flexible-content/accordion.blade.php
+++ web/app/themes/wraith/resources/views/partials/flexible-content/accordion.blade.php
@@ -30,41 +30,17 @@ $custom_class = get_sub_field('custom_class');
                     <h4 class="accordionTitle p-4 bg-neutral-200 text-tertiary mb-0 z-10 hover:bg-blue-400 hover:text-white cursor-pointer hover:drop-shadow-lg">{{ $title }}</h4>
                     <div class="accordionBody p-4 -mt-2 pt-6 mb-3 z-0 border border-gray-200 rounded text-sm bg-white">{!! $accordion_paragraph !!}</div>
                 </div>
-                @endwhile
-            </div>
-            @endif
-        </div>
-    </div>
-    @if($faq_schema)
-    <div class="relative mx-auto flex lg:py-20 py-12 flex-wrap justify-around ">
-        <div class="lg:w-3/4 w-full lg:mx-0 mx-4 flex flex-wrap justify-around items-center relative z-10">
-            <div class="w-full relative mb-4 md:mb-0 md:pr-8 pr-0 md:w-2/5 z-20">
-                <h3 class="title-display title-display-left text-base text-primary">{!! $subtitle !!}</h3>
-                <h2 class="text-3xl lg:text-4xl text-black mt-4">{{ $title ? $title : "FAQ's" }}</h2>
-                <div class="child-p:text-black text-sm mt-4">{!! $paragraph ? $paragraph : null !!}</div>
-            </div>
-            @if ( have_rows( 'accordion_items' ) )
-            @php
-            $i = 0 ;
-            @endphp
-            <div class="w-full md:w-3/5 z-20">
-                @while ( have_rows( 'accordion_items' ) )
-                @php
-                the_row();
-                $i++;
-                $title = get_sub_field('accordion_title');
-                $accordion_paragraph = get_sub_field('accordion_paragraph');
-                @endphp
+                @if($faq_schema)
                 <div itemscope itemprop="mainEntity" itemtype="https://schema.org/Question" class="accordion {{ $i == 1 ? 'active' : null }}">
                     <h4 itemprop="name" class="accordionTitle p-4 bg-neutral-200 text-tertiary mb-0 z-10 hover:bg-blue-400 hover:text-white cursor-pointer hover:drop-shadow-lg">{{ $title }}</h4>
                     <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
                         <div itemprop="text" class="accordionBody p-4 -mt-2 pt-6 mb-3 z-0 border border-gray-200 rounded text-sm bg-white">{!! $accordion_paragraph !!}</div>
                     </div>
                 </div>
+                @endif
                 @endwhile
             </div>
             @endif
         </div>
     </div>
-    @endif
 </section>
